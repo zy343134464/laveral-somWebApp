52b8fee8d82551725238f505943ef82ef5a02b97
zhangyang <zy343134464@163.com>
2018-01-14 19:49:32 +0800

    登录注册完成

---
+++ webFront/dist/js/custom/membermanage.js
@@ -0,0 +1,17 @@
+$(function () {
+    var table = $('#example1').DataTable({
+        'paging'      : false,
+        'lengthChange': true,
+        'searching'   : false,
+        'ordering'    : false,
+        'info'        : false,
+        'autoWidth'   : true
+    })
+    //显示隐藏列
+    $('.toggle-vis').on('change', function (e) {
+        e.preventDefault();
+        console.log($(this).attr('data-column'));
+        var column = table.column($(this).attr('data-column'));
+        column.visible(!column.visible());
+    });
+})
